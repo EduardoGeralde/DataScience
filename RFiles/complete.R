5af451b968c9e513d0b76da4780afe49967fc0d3
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2016-12-09 17:52:09 -0200

    R functions

---
+++ RFiles/complete.R
@@ -0,0 +1,10 @@
+complete <- function(directory,id=1:332){
+        files <<- dir(directory)
+        data_frame <<- do.call("rbind",lapply(files[id], 
+                function(x) read.csv(paste(directory,x,sep = ''),
+                        stringsAsFactors = FALSE)))
+        data_clean <<- data_frame[complete.cases(data_frame),]
+        num_times <<- aggregate(nitrate~ID,data_clean,length)
+        names(num_times)[2]<-'nobs'
+        num_times
+}
