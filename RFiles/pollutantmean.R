af392482bb3a6b37ce6b49d0fbd2030bda0a8272
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2016-12-18 13:23:36 -0200

    Files from week 1 to 3 added

--- RFiles/pollutantmean.R
+++ RFiles/pollutantmean.R
@@ -1,6 +1,6 @@
 pollutantmean <- function(directory,pollutant,id=1:332){
         files <- dir(directory)
-        data_frame <<- do.call("rbind",lapply(files[id], function(x) read.csv(paste(directory,x,sep = ''),stringsAsFactors = FALSE)))
+        data_frame <<- do.call("rbind",lapply(files[id], function(x) read.csv(paste(directory,"/",x,sep = ''),stringsAsFactors = FALSE)))
         data_clean <<- data_frame[complete.cases(data_frame),]
         mean_pollut <<- data_clean[,pollutant]
         mean(mean_pollut)
